16d1643ebb9f4f929e491e2241f31d039fe5e648
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-11-27 00:52:22 -0500

    modal fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,13 @@
 var friends = [
 	{
-		name: 'Rachel',
+		name: 'Arwen',
 		photo: 'https://vignette.wikia.nocookie.net/lotr/images/d/dd/100_beautiful_arwen.jpg/revision/latest?cb=20110313201140',
 		scores: [1, 3, 3, 1, 5, 5, 5, 5, 1, 4]
+	}, 
+	{
+		name: 'Aragorn',
+		photo: 'https://vignette.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423',
+		scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 5]
 	}
 ]
 
